720513031b7b536e5bfa1e28b9b0862981eeb05b
rodriibalondeoro <rmedinaparedes@al.uloyola.es>
2025-04-10 18:49:43 +0200

    Update views.py

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from cuoora.models import Question
+from cuoora.models import Question, Answer, Description
+
 
 def home(request):
     questions = Question.objects.all()
-    context = {"questions": questions}
+    answers = Answer.objects.all()
+    descriptions = Description.objects.all()
+    context = {"questions": questions,
+               "answers": answers,
+               "descriptions": descriptions
+                }
 
     return render(request, 'home.html', context)
 
